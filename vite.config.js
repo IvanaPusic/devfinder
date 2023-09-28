c063d35fcbfbca8f0d379f7478e6f9decf6ad30b
Ivana Pusic <pusicka97@gmail.com>
2023-09-28 16:48:43 +0200

    setup for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
+  };
+
+  if (command !== 'serve') {
+    config.base = '/devfinder/';
+  }
+
+  return config;
+});
